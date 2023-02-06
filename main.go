fb29c00841b5bfc9ef43df09de5eed70c29c4390
Elijah Allen <56412012+eaallen@users.noreply.github.com>
2023-02-06 07:12:54 -0700

    two buttons

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"gioui.org/app"
@@ -13,6 +15,76 @@ import (
 	"gioui.org/widget/material"
 )
 
+type Context = layout.Context
+type Dimensions = layout.Dimensions
+
+func draw(w *app.Window) error {
+	// ops are the operations from the UI
+	var ops op.Ops
+
+	// startButton is a clickable widget
+	var startButton widget.Clickable
+	var stopButton widget.Clickable
+
+	// th defines the material design style
+	th := material.NewTheme(gofont.Collection())
+
+	// listen for events in the window.
+	for e := range w.Events() {
+
+		// detect what type of event
+		switch e := e.(type) {
+
+		// this is sent when the application should re-render.
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+			// Let's try out the flexbox layout concept:
+			layout.Flex{
+				// Vertical alignment, from top to bottom
+				Axis: layout.Vertical,
+				// Empty space is left at the start, i.e. at the top
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+				// We insert two rigid elements:
+				// First a button ...
+				layout.Rigid(
+					func(gtx Context) Dimensions {
+						btn := material.Button(th, &startButton, "Start Server")
+						return btn.Layout(gtx)
+					},
+				),
+				// ... then an empty spacer
+				layout.Rigid(
+					// The height of the spacer is 25 Device independent pixels
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+				layout.Rigid(
+					func(gtx Context) Dimensions {
+						btn := material.Button(th, &stopButton, "End Server")
+						return btn.Layout(gtx)
+					},
+				),
+				layout.Rigid(
+					// The height of the spacer is 25 Device independent pixels
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+			)
+
+			if startButton.Clicked() {
+				fmt.Println("start server")
+			}
+			if stopButton.Clicked() {
+				fmt.Println("Stop Server")
+			}
+
+			e.Frame(gtx.Ops)
+		case system.DestroyEvent:
+			return e.Err
+		}
+	}
+	return nil
+}
+
 func main() {
 	go func() {
 		// create new window
@@ -21,48 +93,11 @@ func main() {
 			app.Size(unit.Dp(400), unit.Dp(600)),
 		)
 
-		// ops are the operations from the UI
-		var ops op.Ops
-
-		// startButton is a clickable widget
-		var startButton widget.Clickable
-
-		// th defines the material design style
-		th := material.NewTheme(gofont.Collection())
-
-		// listen for events in the window.
-		for e := range w.Events() {
-
-			// detect what type of event
-			switch e := e.(type) {
-
-			// this is sent when the application should re-render.
-			case system.FrameEvent:
-				gtx := layout.NewContext(&ops, e)
-				// Let's try out the flexbox layout concept:
-				layout.Flex{
-					// Vertical alignment, from top to bottom
-					Axis: layout.Vertical,
-					// Empty space is left at the start, i.e. at the top
-					Spacing: layout.SpaceStart,
-				}.Layout(gtx,
-					// We insert two rigid elements:
-					// First a button ...
-					layout.Rigid(
-						func(gtx layout.Context) layout.Dimensions {
-							btn := material.Button(th, &startButton, "Start")
-							return btn.Layout(gtx)
-						},
-					),
-					// ... then an empty spacer
-					layout.Rigid(
-						// The height of the spacer is 25 Device independent pixels
-						layout.Spacer{Height: unit.Dp(25)}.Layout,
-					),
-				)
-				e.Frame(gtx.Ops)
-			}
+		err := draw(w)
+		if err != nil {
+			log.Fatal(err)
 		}
+
 		os.Exit(0)
 	}()
 	app.Main()
