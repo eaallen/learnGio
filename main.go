f25417593206132f1f0af27e4797c46eb4928f68
Elijah Allen <56412012+eaallen@users.noreply.github.com>
2023-02-06 05:11:42 -0700

    button

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"os"
 
 	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
 	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
 )
 
 func main() {
@@ -15,8 +21,28 @@ func main() {
 			app.Size(unit.Dp(400), unit.Dp(600)),
 		)
 
+		// ops are the operations from the UI
+		var ops op.Ops
+
+		// startButton is a clickable widget
+		var startButton widget.Clickable
+
+		// th defines the material design style
+		th := material.NewTheme(gofont.Collection())
+
 		// listen for events in the window.
-		for range w.Events() {
+		for e := range w.Events() {
+
+			// detect what type of event
+			switch e := e.(type) {
+
+			// this is sent when the application should re-render.
+			case system.FrameEvent:
+				gtx := layout.NewContext(&ops, e)
+				btn := material.Button(th, &startButton, "Start")
+				btn.Layout(gtx)
+				e.Frame(gtx.Ops)
+			}
 		}
 		os.Exit(0)
 	}()
